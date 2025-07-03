5b52f4087e60432b0b3233ad1fd78441cbc3c289
Atul <dhuriatu@gmail.com>
2025-07-03 09:54:46 +0530

    API URL added static IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://172.31.14.195:3000/api' // Use HTTPS for production
-}; 
+  apiUrl: 'https://13.201.164.188:3000/api' // Use HTTPS for production Elastic IP is used
+};
